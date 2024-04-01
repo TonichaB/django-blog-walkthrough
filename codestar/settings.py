9b35dfa602b9c1baad7cda2fc8af817e127980f8
TonichaB <tonicha.blackwell97@gmail.com>
2024-04-01 17:14:11 +0000

    Implement Comment model definition

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-tonichab-django-blog-wal-a55w6tdt8q.us1.codeanyapp.com',
